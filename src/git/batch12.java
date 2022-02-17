b6a6e5fce2b630a87d4f9e70c94ed207d0d7953e
Samina-Razzaq <s.razzaq2012@gmail.com>
2022-02-16 18:40:03 -0800

    i have updated some codes

--- src/git/batch12.java
+++ src/git/batch12.java
@@ -5,6 +5,8 @@ public class batch12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("we are good");
+
+System.out.println(" good good");
 	}
 
 }
